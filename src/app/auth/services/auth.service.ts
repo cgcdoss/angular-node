47af8581d4ed066fb0397f413346d1f728cd9cb2
cgcdoss <gabi04061997@gmail.com>
2021-09-01 00:18:51 -0300

    Usando BehavioSubject para fazer refreshToken

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 
 
 @Injectable()
 export class AuthService {
+
+  refreshToken$: BehaviorSubject<{ refreshToken: string, user: string }> = new BehaviorSubject({} as any);
+
   constructor(private _http: HttpClient) { }
 
   login(user: string, password: string): Observable<{ token: string; auth: boolean; refreshToken: string }> {
@@ -26,14 +29,27 @@ export class AuthService {
   }
 
   refreshToken(): void {
-    setInterval(() => {
+    let interval = setInterval(() => {
+      if (sessionStorage.getItem('refreshToken') && sessionStorage.getItem('usuario')) {
+        this.refreshToken$.next({
+          refreshToken: sessionStorage.getItem('refreshToken') as string,
+          user: sessionStorage.getItem('usuario') as string
+        });
+      } else {
+        this.refreshToken$.complete();
+        clearInterval(interval);
+      }
+    }, 5 * 60 * 1000); // a cada 5 minutos
+
+
+    this.refreshToken$.subscribe(token => {
       this._http.post<{ token: string }>('http://localhost:3333/api/token', {
-        refreshToken: sessionStorage.getItem('refreshToken'),
-        user: sessionStorage.getItem('usuario')
+        refreshToken: token.refreshToken,
+        user: token.user
       }).subscribe(resp => {
         document.cookie = `token=${resp.token}; max-age=${5 * 60}`;
       });
-    }, 5 * 60 * 1000); // a cada 5 minutos
+    });
   }
 
   getCookie(cookie: string): string {
