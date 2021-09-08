8feeed00653995a77d79b05109908f897fade499
cgcdoss <gabi04061997@gmail.com>
2021-09-08 20:29:37 -0300

    Usando ng-template e ng-container

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { retry, tap, timeout } from 'rxjs/operators';
 import { AuthService } from '../auth/services/auth.service';
@@ -9,14 +9,18 @@ import { HomeService } from './services/home.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
+  @ViewChild('template', { read: TemplateRef }) template!: TemplateRef<any>;
+  @ViewChild('template2', { read: TemplateRef }) template2!: TemplateRef<any>;
 
   clientes!: any[];
+  templateAtual!: TemplateRef<any>;
 
   constructor(
     private _authService: AuthService,
     private _router: Router,
-    private _homeService: HomeService
+    private _homeService: HomeService,
   ) { }
 
   ngOnInit(): void {
@@ -28,15 +32,34 @@ export class HomeComponent implements OnInit {
       )
       .subscribe((res: any) => {
         this.clientes = res;
+        this.container.createEmbeddedView(this.template, { $implicit: this.clientes[0].nome }); // uma forma de fazer o createEmbeddedView
       }, err => {
         console.log('falha ao recuperar clientes', err.error);
       });
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.templateAtual = this.template;
+      
+      // Outra forma de fazer o createEmbeddedView
+      const view = this.template.createEmbeddedView({ $implicit: 'Hello World' });
+      this.container.insert(view);
+    });
   }
 
   logout(): void {
     this._authService.logout().subscribe(resp => {
       this._router.navigateByUrl('/login');
     });
+  }
+
+  trocaTemplate(): void {
+    if (this.templateAtual === this.template) {
+      this.templateAtual = this.template2;
+    } else {
+      this.templateAtual = this.template;
+    }
   }
 
 }
